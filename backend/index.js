d2ddbe8052a13b271b9d0ac8dd5f86e99f63976b
Emilio Rivera <emiliorivera747@gmail.com>
2025-06-02 19:57:06 -0700

    Displaying mesage

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
-const express = require('express');
-  const cors = require('cors');
-  const app = express();
-  const port = 3000;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
 
-  app.use(cors());
-  app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-  app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
-  });
-  app.listen(port, () => {
-    console.log(`Backend server running at http://localhost:${port}`);
-  });
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
+});
+
+app.get("/api/hello-world", (req, res) => {
+  try {
+    res.status(200).json({ message: "Hello, World!" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Backend server running at http://localhost:${port}`);
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState, useEffect} from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [message, setMessage] = useState<string | null>(null);
+
+  useEffect(()=>{
+    const fetchMessage = async () => {
+      try {
+        const response = await fetch('/api/hello-world');
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
+        setMessage(data.message);
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+    };
+
+    fetchMessage();
+  },[]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {message? message : 'Loading...'}
+    </div>
   )
 }
 
