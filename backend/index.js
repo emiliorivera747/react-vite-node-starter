7b38125590f26d553eb0f9ab5dba9e0de26e908e
Emilio Rivera <emiliorivera747@gmail.com>
2025-06-03 10:28:11 -0700

    Install Prisma

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +17,12 @@ app.get("/api/hello-world", (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
+});
+
+app.post('/api/user', (req, res) => {
+  const { username, email } = req.body;
+  if (!username || !email) return res.status(400).json({ error: "Username and email are required" });
+  res.status(201).json({ message: "User created successfully", user: { username, email } });
 });
 
 app.listen(port, () => {
